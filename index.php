d1f605bd6584617d3c7d9c5f056a136c09734d0b
Riley de Man <rileydeman@gmail.com>
2024-11-05 20:40:41 +0100

    Page title update
    Page title update

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (isset($_GET['query'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cyberpunk Music Artist Search</title>
+    <title>TuneTribe the Music Artists Database</title>
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
