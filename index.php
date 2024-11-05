efce03039f8389b0578bf7da70d30bbca4e5ee69
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-11-05 12:28:56 +0100

    More and easier search results

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 // index.php
 require 'includes/db.php';
 
-$artistInfo = null;
+$artistsInfo = [];
 if (isset($_POST['artist'])) {
     $artist = $_POST['artist'];
     $stmt = $pdo->prepare("SELECT * FROM artists WHERE name LIKE ?");
     $stmt->execute(["%$artist%"]);
-    $artistInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+    $artistsInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
 
@@ -28,11 +28,13 @@ if (isset($_POST['artist'])) {
     </form>
 
     <div class="artist-info">
-        <?php if ($artistInfo): ?>
-            <h2><?php echo htmlspecialchars($artistInfo['name']); ?></h2>
-            <p><strong>Genre:</strong> <?php echo htmlspecialchars($artistInfo['genre']); ?></p>
-            <p><?php echo htmlspecialchars($artistInfo['bio']); ?></p>
-            <img src="<?php echo $artistInfo['image_url']; ?>" alt="Artist image">
+        <?php if (!empty($artistsInfo)): ?>
+            <?php foreach ($artistsInfo as $artistInfo): ?>
+                <h2><?php echo htmlspecialchars($artistInfo['name']); ?></h2>
+                <p><strong>Genre:</strong> <?php echo htmlspecialchars($artistInfo['genre']); ?></p>
+                <p><?php echo htmlspecialchars($artistInfo['bio']); ?></p>
+                <img src="<?php echo $artistInfo['image_url']; ?>" alt="Artist image">
+            <?php endforeach; ?>
         <?php else: ?>
             <p>No artist found. Try searching for another artist.</p>
         <?php endif; ?>
